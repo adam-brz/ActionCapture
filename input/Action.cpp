12a21559d3a2ea7e08025bead9212507cdb41766
Andrew2a1 <adam.brz@onet.eu>
2020-11-28 14:28:10 +0100

    Move action start time to Action class

---
+++ input/Action.cpp
@@ -0,0 +1,11 @@
+#include "Action.h"
+
+unsigned Action::getStartTime() const
+{
+    return startTime;
+}
+
+void Action::setStartTime(unsigned value)
+{
+    startTime = value;
+}
--- input/Action.h
+++ input/Action.h
@@ -5,10 +5,15 @@
 
 class Action
 {
+    unsigned startTime = 0;
+
 public:
     virtual ~Action() = default;
     virtual std::string name() const = 0;
     virtual void run() = 0;
+
+    unsigned getStartTime() const;
+    void setStartTime(unsigned value);
 };
 
 #endif // ACTION_H
